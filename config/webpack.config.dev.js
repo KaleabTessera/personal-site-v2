e354fb58f81aeb8bd0192c0ece57ba7bc3089cf6
Kale-ab <kale-ab.tessera@entelect.co.za>
2018-10-15 12:02:29 +0200

    Updated webpack config to allow for markdwon files

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -303,6 +303,14 @@ module.exports = {
               "sass-loader"
             )
           },
+          {
+            test: /\.md$/,
+            use: [
+              {
+                loader: "raw-loader"
+              }
+            ]
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -388,6 +388,14 @@ module.exports = {
               "sass-loader"
             )
           },
+          {
+            test: /\.md$/,
+            use: [
+              {
+                loader: "raw-loader"
+              }
+            ]
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
