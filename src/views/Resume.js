6b9f7f9dcf40e32cb049b2ef245327336562e8cf
Kale-ab <kale-ab.tessera@entelect.co.za>
2018-10-18 08:18:17 +0200

    Added CV.

--- src/views/Resume.js
+++ src/views/Resume.js
@@ -1,22 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 import Helmet from "react-helmet";
 
 import Main from "../layouts/Main";
 
-import Education from "../components/Resume/Education";
-import Experience from "../components/Resume/Experience";
 import Skills from "../components/Resume/Skills";
 import Courses from "../components/Resume/Courses";
 import References from "../components/Resume/References";
 
 import courses from "../data/resume/courses";
-import degrees from "../data/resume/degrees";
-import positions from "../data/resume/positions";
 import { skills, categories } from "../data/resume/skills";
 
-const sections = ["Education", "Experience", "Skills", "Courses", "References"];
+const sections = ["Skills", "Courses", "References"];
 
 const Resume = () => (
   <Main>
@@ -36,8 +32,9 @@ const Resume = () => (
           </div>
         </div>
       </header>
-      <Education data={degrees} />
-      <Experience data={positions} />
+      <NavLink to={"cv_latest.pdf"} target="_blank" className="nav-link">
+        Resume
+      </NavLink>
       <Skills skills={skills} categories={categories} />
       <Courses data={courses} />
       <References />
