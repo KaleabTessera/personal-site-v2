7d61e4085a548d69e35ea70fa4f6345761fa80a3
Kale-ab <kale-ab.tessera@entelect.co.za>
2018-10-19 10:34:57 +0200

    Removed comments.

--- src/App.js
+++ src/App.js
@@ -26,36 +26,6 @@ if (params.search("path=") > -1) {
 }
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     foo: "bar",
-  //     resumeData: {}
-  //   };
-
-  //   ReactGA.initialize("UA-110570651-1");
-  //   ReactGA.pageview(window.location.pathname);
-  // }
-
-  // getResumeData() {
-  //   $.ajax({
-  //     url: "/resumeData.json",
-  //     dataType: "json",
-  //     cache: false,
-  //     success: function(data) {
-  //       this.setState({ resumeData: data });
-  //     }.bind(this),
-  //     error: function(xhr, status, err) {
-  //       console.log(err);
-  //       alert(err);
-  //     }
-  //   });
-  // }
-
-  // componentDidMount() {
-  //   this.getResumeData();
-  // }
-
   render() {
     return (
       <Router>
