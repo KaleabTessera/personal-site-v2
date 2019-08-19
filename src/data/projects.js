ace53895cc853b8700338593a403ebd25e759c46
Kale-ab <kaleabtessera@gmail.com>
2019-08-19 14:19:19 +0200

    Updated projects page.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,24 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: "Baobab",
+    subtitle: "Software Development",
+    link: 'https://baobab.deeplearningindaba.com',
+    image: "/images/projects/Baobab.png",
+    date: "2019-08-25",
+    desc: "I was the Front-end Architect for the Deep Learning Indaba applications and selection web application. This included functionality such as applying to the conference, creating a profile and assigning and reviwing applications."
+  },
+  {
+    title: "Turtlebot Kuri-Navigation",
+    subtitle: "Robotics",
+    link: 'https://github.com/KaleabTessera/Turtlebot-Kuri-Navigation',
+    image: "/images/projects/Kuri.jpg",
+    date: "2019-06-25",
+    desc: "A Turtlebot and Kuri (Robot) were both given a challenge of navigating an obstacle course (containing boxes and cones). This was done to test whose navigation and localization was better in a static and dynamic environment (dynamic refers to when both robots are navigating the obstacle course together)."
+  },
+  {
     title: "ICO Omen",
-    subtitle: "2015 BVP Hackathon",
+    subtitle: "Machine Learning",
     link: 'https://towardsdatascience.com/icoomen-using-machine-learning-to-predict-ico-prices-29fa4cec6d86',
     image: "/images/projects/IcoOmen.jpeg",
     date: "2018-12-15",
